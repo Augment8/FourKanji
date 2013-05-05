fd3cf8cb94ea3ab9c026a424e7632da121677223
Tomohiko Himura <eiel.hal@gmail.com>
2013-05-06 00:44:42 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FourKanji::Application.config.secret_key_base = '59f8bd1fac7257ff9b7eba8ee0fb914aecb34356e7a861fa0779bce0719f69ade9d0e5a96ee56988ffaaff82b83590ce6dc90730f697a682e561e6fd0efd34af'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FourKanji::Application.config.session_store :cookie_store, key: '_FourKanji_session'
